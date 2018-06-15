f1cf1e5c7892da7038bf3b488fc5dbafdd315145
unknown <oscar.valencia@mobileappscompany.us>
2018-06-14 22:41:06 -0400

    complete

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="0c77e2e6-f5a6-46ae-9779-e4f99f2c61bd")
+public class DataBindingInfo {}
